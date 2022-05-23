2b20c19020342bd5b3c09aa0c107f26770aa541c
Cory J. Cascalheira <corycaaz@gmail.com>
2022-05-23 10:12:42 -0600

    Finish feature importance plots

--- src/classification/visualize_feature_importance.R
+++ src/classification/visualize_feature_importance.R
@@ -10,32 +10,43 @@ feat_importance <- read_csv("data/results/feature_selection/avg_feature_importan
   arrange(desc(info_gained))
 
 # Visualize the number of features selected
-feat_selected %>%
-  mutate(category = recode(category, "clinical_keywords" = "Clinical Keywords", "n_grams" = "n-grams", 
+feat_selected_plot <- feat_selected %>%
+  mutate(category = recode(category, "clinical_keywords" = "Clinical Keywords", "n_grams" = "n-Grams", 
                            "psycholinguistic" = "Psycholinguistic\nAttributes", "sentiment_valence" = "Emotional Valence\n(Sentiment)",
                            "embedding" = "Word Emeddings", "dass" = "Psychological\nDistress (DASS)")) %>%
   mutate(percent_used = round(percent_used, 2)) %>%
   ggplot(aes(x = reorder(category, percent_used), y = percent_used)) +
   geom_bar(stat="identity", fill = "dodgerblue3") +
-  coord_flip() +
+  coord_flip(clip = "off") +
   scale_y_continuous(expand = c(0, 0)) +
   labs(
     x = "Independent Variable Categories",
     y = "Percent Used in XGBoost Classifier"
   ) +
-  theme_bw()
+  theme_bw() +
+  theme(text=element_text(family="serif"))
+feat_selected_plot
 
 # Visualize the average information gained
-feat_importance %>%
-  mutate(category = recode(category, "clinical_keywords" = "Clinical Keywords", "n_grams" = "n-grams", 
+feat_importance_plot <- feat_importance %>%
+  mutate(category = recode(category, "clinical_keywords" = "Clinical Keywords", "n_grams" = "n-Grams", 
                            "psycholinguistic" = "Psycholinguistic\nAttributes", "sentiment_valence" = "Emotional Valence\n(Sentiment)",
                            "embedding" = "Word Emeddings", "dass" = "Psychological\nDistress (DASS)")) %>%
   ggplot(aes(x = reorder(category, info_gained), y = info_gained)) +
   geom_bar(stat="identity", fill = "dodgerblue3") +
-  coord_flip() +
+  coord_flip(clip = "off") +
   scale_y_continuous(expand = c(0, 0), n.breaks = 10, limits = c(0, 35)) +
   labs(
     x = "Independent Variable Categories",
     y = "Average Information Gained (Gini Index)"
   ) +
-  theme_bw()
+  theme_bw() +
+  theme(text=element_text(family="serif"))
+feat_importance_plot
+
+# Export plots
+ggsave("data/results/plots/feat_selected_plot.png", plot = feat_selected_plot,
+       width = 5.0, height = 4.0)
+
+ggsave("data/results/plots/feat_importance_plot.png", plot = feat_importance_plot,
+       width = 5.0, height = 4.0)
